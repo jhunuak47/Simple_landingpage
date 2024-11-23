99340c3240546e5555ae09c039693ccbe8497842
Rohan <jhunu1201@gmail.com>
2024-11-23 13:33:25 +0530

    Update build and serve configuration for Heroku

---
+++ frontend/build/precache-manifest.5fcb846bb63a590de903835751645467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445839a527b26d8476ec4bf57f8c5ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e0b6771c55cca5cac12",
+    "url": "/static/css/main.9bfd7d73.chunk.css"
+  },
+  {
+    "revision": "3c62f6194834434b7cfc",
+    "url": "/static/js/2.9e0db0a8.chunk.js"
+  },
+  {
+    "revision": "0887163a8605841c3eb9334a885cc238",
+    "url": "/static/js/2.9e0db0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5409af3f265962197e63",
+    "url": "/static/js/3.6933d5cf.chunk.js"
+  },
+  {
+    "revision": "9e0b6771c55cca5cac12",
+    "url": "/static/js/main.0f7240a7.chunk.js"
+  },
+  {
+    "revision": "cc0e103a9b5f70bafc57",
+    "url": "/static/js/runtime-main.6c9cd047.js"
+  }
+]);
